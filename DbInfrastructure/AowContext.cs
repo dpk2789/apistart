049e4ac4bed882c7cf1ff773010c202cfa8d922a
deepakkhemchandani27 <deepakkhemchandani27@gmail.com>
2022-12-19 20:05:31 +0530

    web api and infrastructure added

---
+++ DbInfrastructure/AowContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbInfrastructure
+{
+    public class AowContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+    {
+        public AowContext(DbContextOptions<AowContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ DbInfrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace DbInfrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AowContext>(options => options.UseSqlServer(connectionString).EnableSensitiveDataLogging());                   
+            return services;
+        }
+    }
+}
